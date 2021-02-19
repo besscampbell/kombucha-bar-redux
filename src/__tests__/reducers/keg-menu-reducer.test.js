5cc604a0ecb70fb5f3000db991b536a2d2d47ec3
Bess Campbell <bess.k.campbell@gmail.com>
2021-02-19 09:05:01 -0800

    Show update test for Add keg is passing

--- src/__tests__/reducers/keg-menu-reducer.test.js
+++ src/__tests__/reducers/keg-menu-reducer.test.js
@@ -12,6 +12,26 @@ describe('kegMenuReducer', () => {
     pintsRemaining: 12,
     id: 1
   }
+  const currentState = {
+    1: {
+    brand: 'Brew Dr.',
+    flavor: 'Uplift',
+    pricePerPint: 3.50,
+    kegPrice: 270.00,
+    untappedKegs: 1,
+    pintsRemaining: 12,
+    id: 1
+    },
+    2: {
+    brand: 'GTs Synergy',
+    flavor: 'Trilogy',
+    pricePerPint: 3.75,
+    kegPrice: 315.00,
+    untappedKegs: 0,
+    pintsRemaining: 99,
+    id: 2
+    }
+  }
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegMenuReducer({}, {type: null})).toEqual({});
   });
@@ -40,4 +60,38 @@ describe('kegMenuReducer', () => {
       }
     });
   });
+
+  test('Should correctly update keg using the ADD KEG action', () => {
+    const { brand, flavor, pricePerPint, kegPrice, untappedKegs, pintsRemaining, id } = kegInfo;
+    action = {
+      type: 'ADD_KEG',
+      brand: 'Brew Dr.',
+      flavor: 'Uplift',
+      pricePerPint: 3.50,
+      kegPrice: 270.00,
+      untappedKegs: 0,
+      pintsRemaining: 124,
+      id: 1
+    }
+    expect(kegMenuReducer(currentState, action)).toEqual({
+      1: {
+        brand: 'Brew Dr.',
+        flavor: 'Uplift',
+        pricePerPint: 3.50,
+        kegPrice: 270.00,
+        untappedKegs: 0,
+        pintsRemaining: 124,
+        id: 1
+        },
+        2: {
+        brand: 'GTs Synergy',
+        flavor: 'Trilogy',
+        pricePerPint: 3.75,
+        kegPrice: 315.00,
+        untappedKegs: 0,
+        pintsRemaining: 99,
+        id: 2
+        }
+    });
+  })
 });
