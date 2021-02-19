d19e4b750486b6bfdb400d6ec8ead08b6a87cf6f
Bess Campbell <bess.k.campbell@gmail.com>
2021-02-19 10:59:58 -0800

    Show that root/formview initial state test is passing

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,10 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formViewReducer from '../../reducers/form-view-reducer';
+import kegMenuReducer from '../../reducers/keg-menu-reducer';
+import selectedKegReducer from '../../reducers/selected-keg-reducer';
+
+let store = createStore(rootReducer);
 
 describe('rootReducer', () => {
   test('Should return default state if no action type is recognized', () => {
@@ -7,5 +13,9 @@ describe('rootReducer', () => {
       selectedKeg: null,
       kegMenu: {},
     });
+  });
+
+  test('Check that initial stat of formViewReducer matches root reducer', () => {
+    expect(store.getState().formView).toEqual(formViewReducer(undefined, {type: null}));
   });
 });
