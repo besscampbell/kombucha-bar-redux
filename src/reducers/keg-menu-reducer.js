2191b757180598f8795acf5d88f1f36ce946719f
Bess Campbell <bess.k.campbell@gmail.com>
2021-02-19 09:45:33 -0800

    Show toggle form test is passing

--- src/reducers/keg-menu-reducer.js
+++ src/reducers/keg-menu-reducer.js
@@ -1,23 +1,23 @@
 export default (state = {}, action) => {
   const { brand, flavor, pricePerPint, kegPrice, untappedKegs, pintsRemaining, id, type } = action;
   switch(type){
-    case 'ADD_KEG':
-      return Object.assign({}, state, {
-        [id]: {
-          brand,
-          flavor,
-          pricePerPint,
-          kegPrice,
-          untappedKegs,
-          pintsRemaining,
-          id
-        }
-      });
-    case 'DELETE_KEG':
-      let newState = {...state};
-      delete newState[id];
-      return newState;
-    default:
-      return state;
+  case 'ADD_KEG':
+    return Object.assign({}, state, {
+      [id]: {
+        brand,
+        flavor,
+        pricePerPint,
+        kegPrice,
+        untappedKegs,
+        pintsRemaining,
+        id
+      }
+    });
+  case 'DELETE_KEG':
+    let newState = {...state};
+    delete newState[id];
+    return newState;
+  default:
+    return state;
   }
 };
