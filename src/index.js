337fe3af6497883fedaab96949795d33c0aa978b
Bess Campbell <bess.k.campbell@gmail.com>
2021-02-19 10:44:19 -0800

    Add redux to project

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+import formViewReducer from './form-view-reducer';
+import kegMenuReducer from './keg-menu-reducer';
+import selectedKegReducer from './selected-keg-reducer';
+import { combineReducers } from 'redux';
