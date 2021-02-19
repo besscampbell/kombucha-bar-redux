d553a0e7870d7bc0267c7499e39bef78ccb129cf
Bess Campbell <bess.k.campbell@gmail.com>
2021-02-19 10:01:57 -0800

    Show test for SELECT KEG is failing

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -1,8 +1,49 @@
 import selectedKegReducer from '../../reducers/selected-keg-reducer';
 
 describe('selectedKegReducer', () => {
+
+  let action;
+
+  const kegMenu = {
+    1: {
+    brand: 'Brew Dr.',
+    flavor: 'Uplift',
+    pricePerPint: 3.50,
+    kegPrice: 270.00,
+    untappedKegs: 1,
+    pintsRemaining: 12,
+    id: 1
+    },
+    2: {
+    brand: 'GTs Synergy',
+    flavor: 'Trilogy',
+    pricePerPint: 3.75,
+    kegPrice: 315.00,
+    untappedKegs: 0,
+    pintsRemaining: 99,
+    id: 2
+    }
+  }
   test('Should return default state if no action type is passed in', () => {
-    expect(selectedKegReducer({}, {type: null })).toEqual({});
+    expect(selectedKegReducer(null, {type: null })).toEqual(null);
   });
-  
+
+  test('Should correctly return selected keg', () => {
+    action = {
+      type: 'SELECT_KEG',
+      id: 1,
+      kegMenu: kegMenu
+    }
+    expect(selectedKegReducer(null, action)).toEqual(
+        {
+        brand: 'Brew Dr.',
+        flavor: 'Uplift',
+        pricePerPint: 3.50,
+        kegPrice: 270.00,
+        untappedKegs: 1,
+        pintsRemaining: 12,
+        id: 1
+        }
+    );
+  });
 });
