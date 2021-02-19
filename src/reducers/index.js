4b638623067de4dca4c23cb7e05ec378bbefdf1b
Bess Campbell <bess.k.campbell@gmail.com>
2021-02-19 10:46:05 -0800

    Show root reducer test is failing

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,3 +2,7 @@ import formViewReducer from './form-view-reducer';
 import kegMenuReducer from './keg-menu-reducer';
 import selectedKegReducer from './selected-keg-reducer';
 import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+
+});
