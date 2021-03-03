cd25326be60adf327569028ebf31ff00d1fe05a6
Bess Campbell <bess.k.campbell@gmail.com>
2021-03-03 10:08:00 -0800

    Convert control to func component

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -6,11 +6,11 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import * as a from './../actions';
 
-class KegControl extends React.Component {
+function KegControl(props) {
 
-  handleClick = () =>{
-    const { dispatch } = this.props;
-    if(this.props.selectedKeg != null){
+  const handleClick = () =>{
+    const { dispatch } = props;
+    if(props.selectedKeg != null){
       const action = a.deselect();
       dispatch(action);
     } else {
@@ -19,92 +19,92 @@ class KegControl extends React.Component {
     }
   }
 
-  handleAddNewKeg = (newKeg) => {
-    const { dispatch } = this.props;
+  const handleAddNewKeg = (newKeg) => {
+    const { dispatch } = props;
     const action = a.addKeg(newKeg);
     dispatch(action);
     const action2 = a.toggleForm();
     dispatch(action2);
   }
 
-  handleDeleteKeg = (id) => {
-    const { dispatch } = this.props;
+  const handleDeleteKeg = (id) => {
+    const { dispatch } = props;
     const action = a.deleteKeg(id);
     dispatch(action);
     const action2 = a.deselect();
     dispatch(action2);
   }
 
-  handleKegInView = (id) => {
-    const { dispatch } = this.props;
+  const handleKegInView = (id) => {
+    const { dispatch } = props;
     const action = a.selectKeg(id);
     dispatch(action);
   }
 
-  handleRestock = () => {
-    const { dispatch } = this.props;
-    const kegId = this.props.selectedKeg;
-    const keg = this.props.kegMenu[kegId];
+  const handleRestock = () => {
+    const { dispatch } = props;
+    const kegId = props.selectedKeg;
+    const keg = props.kegMenu[kegId];
     const oneMoreKeg = (keg.untappedKegs + 1);
     const action = a.addKeg({...keg, untappedKegs: oneMoreKeg});
     dispatch(action);
   }
 
-  handleBuy = () => {
-    const { dispatch } = this.props;
-    const kegId = this.props.selectedKeg;
-    const keg = this.props.kegMenu[kegId];
+  const handleBuy = () => {
+    const { dispatch } = props;
+    const kegId = props.selectedKeg;
+    const keg = props.kegMenu[kegId];
     const oneLessPint = keg.pintsRemaining - 1
     const action = a.addKeg({...keg, pintsRemaining: oneLessPint});
     dispatch(action);
   }
 
-  handleTapNewKeg = () => {
-    const { dispatch } = this.props;
-    const kegId = this.props.selectedKeg;
-    const keg = this.props.kegMenu[kegId];
+  const handleTapNewKeg = () => {
+    const { dispatch } = props;
+    const kegId = props.selectedKeg;
+    const keg = props.kegMenu[kegId];
     const tappedKeg = (keg.untappedKegs - 1);
     const action = a.addKeg({...keg, untappedKegs: tappedKeg, pintsRemaining: 124})
     dispatch(action);
   }
 
-  render(){
+  // {
     let visibleNow = null;
     let buttonText = null;
-    if(this.props.selectedKeg != null){
-      const selectedKeg = this.props.kegMenu[this.props.selectedKeg];
+    if(props.selectedKeg != null){
+      const selectedKeg = props.kegMenu[props.selectedKeg];
       visibleNow =
       <KegSpecs
         keg={selectedKeg}
-        onRestock={this.handleRestock}
-        onBuy={this.handleBuy}
-        onTapNewKeg={this.handleTapNewKeg}
-        onDelete={this.handleDeleteKeg}
+        onRestock={handleRestock}
+        onBuy={handleBuy}
+        onTapNewKeg={handleTapNewKeg}
+        onDelete={handleDeleteKeg}
       />
       buttonText="Booch Menu"
-    } else if (!this.props.formView){
+    } else if (!props.formView){
       visibleNow =
       <KegMenu
-        kegMenu={this.props.kegMenu}
-        onKegSelection={this.handleKegInView}
+        kegMenu={props.kegMenu}
+        onKegSelection={handleKegInView}
       />
       buttonText="+ new Booch"
     } else {
       visibleNow =
       <NewKeg
-        onNewKeg={this.handleAddNewKeg}
+        onNewKeg={handleAddNewKeg}
       />
       buttonText="Booch Menu"
     }
 
     return(
       <React.Fragment>
-        <button onClick={this.handleClick}>{buttonText}</button>
+        <button onClick={handleClick}>{buttonText}</button>
         <hr/>
         {visibleNow}
       </React.Fragment>
     );
-  }
+  // }
 }
 
 KegControl.propTypes = {
@@ -121,5 +121,4 @@ const mapStateToProps = state => {
   }
 }
 
-KegControl = connect(mapStateToProps)(KegControl);
-export default KegControl;
+export default connect(mapStateToProps)(KegControl);
